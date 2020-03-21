a0ab8d6d597038145536391d0090795941d73fb6
adiladiletov <nurlubek009>
2020-03-21 11:30:46 +0600

    start examen11

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: 'mongodb://localhost/onlineStore',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+};
---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const config = require('./config');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+const run = async () => {
+    await mongoose.connect(config.database, config.options);
+
+    app.listen(port)
+};
+
+run().catch(error=> {
+    console.error(error)
+});
